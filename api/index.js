a77e4cf2c90b343a8665ecdb9e90d495b0cdc6ee
Jose Uriel Saenz Cuellar <54480651+hackrey21@users.noreply.github.com>
2026-03-27 11:14:55 -0500

    Change API URL from HTTPS to HTTP
    http

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import https from 'https';
 
-const API_IA_URL = "https://softgateia-api.trareysa.com:8096/api/chatbot/ask";
+const API_IA_URL = "http://softgateia-api.trareysa.com:8096/api/chatbot/ask";
 const API_TOKEN  = "SGIA-EMP-bba7729e6dae45eb9d45202b4cbd4b67";
 
 export default async function handler(req, res) {
